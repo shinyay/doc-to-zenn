1ffafe70d1bbcb85a92bfafe84b65e634173182b
shinyay <shinya.com@gmail.com>
2022-08-28 22:09:29 +0900

    Add sample code

--- codes/day_6_guessing_game/src/main.rs
+++ codes/day_6_guessing_game/src/main.rs
@@ -1,3 +1,14 @@
+use std::io;
+
 fn main() {
-    println!("Hello, world!");
+    println!("数を予想しましょう。");
+    println!("予想した値を入力してください。");
+    
+    let mut guess = String::new();
+
+    io::stdin()
+        .read_line(&mut guess)
+        .expect("読み込みに失敗しました。");
+        
+    println!("次のように予測しています: {}", guess);
 }
