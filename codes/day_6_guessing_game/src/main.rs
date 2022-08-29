433049e1263d7917784d7a2a2ceb093d5d0238f9
shinyay <shinya.com@gmail.com>
2022-08-29 18:32:46 +0900

    Add random number generation

--- codes/day_6_guessing_game/src/main.rs
+++ codes/day_6_guessing_game/src/main.rs
@@ -1,7 +1,14 @@
 use std::io;
 
+use rand::Rng;
+
 fn main() {
     println!("数を予想しましょう。");
+
+    let secret_number = rand::thread_rng().gen_range(1..101);
+
+    println!("生成した値: {}", secret_number);
+
     println!("予想した値を入力してください。");
     
     let mut guess = String::new();
