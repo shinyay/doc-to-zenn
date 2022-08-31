7a99619f7e43b349c930aff5f199a44e128b2953
shinyay <shinya.com@gmail.com>
2022-08-31 20:49:19 +0900

    Add shadowing sample

--- codes/day_9_program_concept/src/main.rs
+++ codes/day_9_program_concept/src/main.rs
@@ -4,4 +4,15 @@ fn main() {
     println!("xの値は {} です", x);
     x = "Hello Rust";
     println!("xの値は {} です", x);
+
+    // Checking Shadowing
+    let x = 1;
+    let x = x + 2;
+
+    {
+        let x = x * 3;
+        println!("インナースコープの値: {}", x);
+    }
+
+    println!("アウタースコープの値: {}", x);
 }
