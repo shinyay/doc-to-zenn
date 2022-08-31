0a41295a7939ab904df436b09b6dd60137a85934
shinyay <shinya.com@gmail.com>
2022-08-31 21:31:33 +0900

    Add tuple

--- codes/day_9_program_concept/src/main.rs
+++ codes/day_9_program_concept/src/main.rs
@@ -8,11 +8,16 @@ fn main() {
     // Checking Shadowing
     let x = 1;
     let x = x + 2;
-
     {
         let x = x * 3;
         println!("インナースコープの値: {}", x);
     }
-
     println!("アウタースコープの値: {}", x);
+
+    // Checking Tuple
+    let tup = (100, 2.3, 4);
+    let (x, y, z) = tup;
+    println!("yの値: {}", y);
+    println!("3番目の値: {}", tup.2);
+
 }
