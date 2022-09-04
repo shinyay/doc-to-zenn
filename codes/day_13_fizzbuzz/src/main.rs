d96d110dd6d7cb001dc2cc80fb659ad56b1dd195
shinyay <shinya.com@gmail.com>
2022-09-04 19:11:50 +0900

    Add basic fizzbuzz

--- codes/day_13_fizzbuzz/src/main.rs
+++ codes/day_13_fizzbuzz/src/main.rs
@@ -1,3 +1,13 @@
 fn main() {
-    println!("Hello, world!");
+    for x in 1..=100 {
+        if x % 3 == 0 && x % 5 == 0 {
+            println!("FizzBuzz")
+        } else if x % 3 == 0 {
+            println!("Fizz")
+        } else if x % 5 == 0 {
+            println!("Buzz")
+        } else {
+            println!("{}", x)
+        }
+    }
 }
