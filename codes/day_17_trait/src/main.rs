f4552fa3343936cded8ad57b8236423d858ce45e
shinyay <shinya.com@gmail.com>
2022-09-08 15:52:44 +0900

    Add struct and its implementation

--- codes/day_17_trait/src/main.rs
+++ codes/day_17_trait/src/main.rs
@@ -5,3 +5,24 @@ fn main() {
 trait FooBarBaz {
     fn doSomething(&self);
 }
+
+struct Foo;
+impl FooBarBaz for Foo {
+    fn doSomething(&self) {
+        println!("Foo");
+    }
+}
+
+struct Bar;
+impl FooBarBaz for Bar {
+    fn doSomething(&self) {
+        println!("Bar");
+    }
+}
+
+struct Baz;
+impl FooBarBaz for Baz {
+    fn doSomething(&self) {
+        println!("Baz");
+    }
+}
