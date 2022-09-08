6a376fea4b71d137abc61e5c70ba08dc5aa79763
shinyay <shinya.com@gmail.com>
2022-09-08 18:34:23 +0900

    Add trait explanation

--- codes/day_17_trait/src/main.rs
+++ codes/day_17_trait/src/main.rs
@@ -8,16 +8,24 @@ fn main() {
     foo.do_something();
     Bar::do_something(&bar);
     FooBarBaz::do_something(&baz);
+    
+    println!("Message: {}", foo.hello());
+    println!("Message: {}", bar.hello());
+    println!("Message: {}", baz.hello());
 }
 
 trait FooBarBaz {
     fn do_something(&self);
+    fn hello(&self) -> &str;
 }
 
 struct Foo;
 impl FooBarBaz for Foo {
     fn do_something(&self) {
         println!("Foo");
+    }
+    fn hello(&self) -> &str {
+        "Hello, Foo"
     }
 }
 
@@ -26,11 +34,17 @@ impl FooBarBaz for Bar {
     fn do_something(&self) {
         println!("Bar");
     }
+    fn hello(&self) -> &str {
+        "Hello, bar"
+    }
 }
 
 struct Baz;
 impl FooBarBaz for Baz {
     fn do_something(&self) {
         println!("Baz");
+    }
+    fn hello(&self) -> &str {
+        "Hello, Baz"
     }
 }
