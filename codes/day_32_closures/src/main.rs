47627fb49147d21eb69fd8630266dd3c9757733b
shinyay <shinya.com@gmail.com>
2022-09-25 06:41:53 +0900

    Add annotated closure

--- codes/day_32_closures/src/main.rs
+++ codes/day_32_closures/src/main.rs
@@ -1,8 +1,12 @@
 fn main() {
 
-    fn do_something(i: u32) -> u32 {
+    fn do_function(i: u32) -> u32 {
         i + 1
     }
 
-    println!("関数: {}", do_something(1));
+    println!("関数: {}", do_function(1));
+
+    let do_closure_annotated = |i: u32| -> u32 { i + 1};
+
+    println!("クロージャ: {}", do_closure_annotated(2));
 }
