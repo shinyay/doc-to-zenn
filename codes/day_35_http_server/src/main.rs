7e48f017d04f2677bd5d52424f66c265985109cb
shinyay <shinya.com@gmail.com>
2022-09-29 14:18:27 +0900

    Add incoming for TcpListener

--- codes/day_35_http_server/src/main.rs
+++ codes/day_35_http_server/src/main.rs
@@ -2,4 +2,9 @@ use std::net::TcpListener;
 
 fn main() {
     let listner = TcpListener::bind("127.0.0.1:8080").unwrap();
+
+    for stream in listner.incoming() {
+        let stream = stream.unwrap();
+        println!("接続確立!");
+    }
 }
