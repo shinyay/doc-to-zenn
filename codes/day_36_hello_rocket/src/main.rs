0ff8ae49c2edeacd533037a646d40dd2f1bfd850
shinyay <shinya.com@gmail.com>
2022-10-01 09:31:55 +0900

    Add Routing

--- codes/day_36_hello_rocket/src/main.rs
+++ codes/day_36_hello_rocket/src/main.rs
@@ -1,3 +1,11 @@
-fn main() {
-    println!("Hello, world!");
+#[macro_use] extern crate rocket;
+
+#[get("/")]
+fn index() -> &'static str {
+    "Hello, Rocket"
+}
+
+#[launch]
+fn rocket() -> _ {
+    rocket::build().mount("/", routes![index])
 }
