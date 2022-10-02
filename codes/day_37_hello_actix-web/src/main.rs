40a5148b3892697491a2940afe81e4aec8af6344
shinyay <shinya.com@gmail.com>
2022-10-02 19:23:14 +0900

    Add main function to run web app

--- codes/day_37_hello_actix-web/src/main.rs
+++ codes/day_37_hello_actix-web/src/main.rs
@@ -1,6 +1,19 @@
-use actix_web::{get, Responder, HttpResponse};
+use std::io::Result;
+
+use actix_web::{get, Responder, HttpResponse, HttpServer, App};
 
 #[get("/")]
 async fn hello() -> impl Responder {
     HttpResponse::Ok().body("Hello actix-web!")
+}
+
+#[actix_web::main]
+async fn main() -> Result<()> {
+    HttpServer::new(|| {
+        App::new()
+            .service(hello)
+    })
+    .bind(("127.0.0.1", 8080))?
+    .run()
+    .await
 }
