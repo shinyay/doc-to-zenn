8ee91ba8a095fa2d54ce4254b3e4b31e3f830b2f
shinyay <shinya.com@gmail.com>
2022-10-05 09:56:55 +0900

    Add socket listener

--- codes/day_39_hello_hyper/src/main.rs
+++ codes/day_39_hello_hyper/src/main.rs
@@ -1,3 +1,23 @@
-fn main() {
-    println!("Hello, world!");
+use std::convert::Infallible;
+use std::net::SocketAddr;
+use hyper::{Body, Request, Response, Server};
+use hyper::service::{make_service_fn, service_fn};
+
+async fn hello_hyper(req: Request<Body>) -> Result<Response<Body>, Infallible> {
+    Ok(Response::new("Hello, hyper".into()))
+}
+
+#[tokio::main]
+async fn main() {
+    let addr = SocketAddr::from(([127, 0, 0, 1], 8080));
+    
+    let make_svc = make_service_fn(|_conn| async {
+        Ok::<_, Infallible>(service_fn(hello_hyper))
+    });
+
+    let server = Server::bind(&addr).serve(make_svc);
+
+    if let Err(e) = server.await {
+        eprintln!("server error: {}", e);
+    }
 }
