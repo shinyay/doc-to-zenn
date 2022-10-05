83fe1246118ad2b56c81f22eba26a7989dfa90fb
shinyay <shinya.com@gmail.com>
2022-10-05 10:03:13 +0900

    Modify syntax for parameter prefix

--- codes/day_39_hello_hyper/src/main.rs
+++ codes/day_39_hello_hyper/src/main.rs
@@ -3,7 +3,7 @@ use std::net::SocketAddr;
 use hyper::{Body, Request, Response, Server};
 use hyper::service::{make_service_fn, service_fn};
 
-async fn hello_hyper(req: Request<Body>) -> Result<Response<Body>, Infallible> {
+async fn hello_hyper(_req: Request<Body>) -> Result<Response<Body>, Infallible> {
     Ok(Response::new("Hello, hyper".into()))
 }
 
