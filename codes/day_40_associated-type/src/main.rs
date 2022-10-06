9fa8f3ebfb6394b0c5c2f6707378a2bb2889a827
shinyay <shinya.com@gmail.com>
2022-10-06 18:41:42 +0900

    Add MVP

--- codes/day_40_associated-type/src/main.rs
+++ codes/day_40_associated-type/src/main.rs
@@ -1,3 +1,30 @@
+struct Point(i32, i32);
+
+trait Position<X, Y> {
+    fn exist(&self, _: &X, _: &Y) -> bool;
+    fn h_axis(&self) -> i32;
+    fn v_axis(&self) -> i32;
+}
+
+impl Position<i32, i32> for Point{
+    fn exist(&self, x: &i32, y: &i32) -> bool {
+        (&self.0 == x) && (&self.1 == y)
+    }
+
+   fn h_axis(&self) -> i32 {
+        self.0
+    }
+
+    fn v_axis(&self) -> i32 {
+        self.1
+    }
+}
+
 fn main() {
-    println!("Hello, world!");
+    let x = 5;
+    let y = 10;
+
+    let point = Point(x, y);
+
+    println!("Point X:{}, Y:{}", x, y);
 }
