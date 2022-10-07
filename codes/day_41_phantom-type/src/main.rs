7881ecbb0a6e8baaf2aeb319d2f391c62de51bd4
shinyay <shinya.com@gmail.com>
2022-10-07 21:36:07 +0900

    Add phatom type sample

--- codes/day_41_phantom-type/src/main.rs
+++ codes/day_41_phantom-type/src/main.rs
@@ -1,10 +1,21 @@
 use std::marker::PhantomData;
 
+#[derive(Debug,PartialEq)]
 struct PhantomStruct<X, A> {
     value: A,
     phantom: PhantomData<X>
 }
 
 fn main() {
+    let _phantom1: PhantomStruct<i32, char> = PhantomStruct {
+        value: 'P', 
+        phantom: PhantomData
+    };
+
+    let _phantom2: PhantomStruct<i64, char> = PhantomStruct {
+        value: 'P', 
+        phantom: PhantomData
+     };
+
     println!("Hello, world!");
 }
