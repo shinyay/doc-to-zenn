8dffdce90e11ef5c0e11a55c702d33caa586bc13
shinyay <shinya.com@gmail.com>
2022-10-18 08:59:02 +0900

    Add unwrap method

--- codes/day_49_error-handling/src/main.rs
+++ codes/day_49_error-handling/src/main.rs
@@ -1,5 +1,20 @@
 fn main() {
+    call_unwrap();
+    // call_panic();
+}
+
+fn call_panic() {
     println!("処理してます - 1");
     panic!("Panicしました！");
     println!("処理してます - 2");
+}
+
+fn call_unwrap() {
+    // OK
+    let x: Result<u32, &str> = Ok(2);
+    assert_eq!(x.unwrap(), 2);
+
+    // ERROR
+    let x: Result<u32, &str> = Err("emergency failure");
+    x.unwrap();
 }
