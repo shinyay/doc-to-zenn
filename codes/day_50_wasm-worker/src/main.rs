2e21479be6469f987cbdebbeec20e228104102ae
shinyay <shinya.com@gmail.com>
2022-10-18 19:21:50 +0900

    Add reply function for worker

--- codes/day_50_wasm-worker/src/main.rs
+++ codes/day_50_wasm-worker/src/main.rs
@@ -1,3 +1,10 @@
-fn main() {
-    println!("Hello, world!");
+use anyhow::Result;
+use wasm_workers_rs::{handler, http::{self, Request, Response}};
+
+#[handler]
+fn reply(req: Request<String>) -> Result<Response<String>> {
+    Ok(http::Response::builder()
+    .status(200)
+    .header("x-generated-by", "wasm-workers-server")
+    .body(String::from("Hello Wasm!").into())?)
 }
