f22e058446e2b08208ae7f4004ebba64b0778a70
shinyay <shinya.com@gmail.com>
2022-10-21 12:21:37 +0900

    Add match

--- codes/day_52_wasm-worker-kv/src/main.rs
+++ codes/day_52_wasm-worker-kv/src/main.rs
@@ -1,8 +1,14 @@
 use anyhow::Result;
 use wasm_workers_rs::{handler, http::{self, Request, Response}, cache::Cache};
 
-#[handler(cache)]
 fn reply(req: Request<String>, cache: &mut Cache) -> Result<Response<String>> {
+
+    let count = cache.get("counter");
+    let count_num = match count {
+        Some(_) => todo!(),
+        None => todo!(),
+    };
+
     Ok(http::Response::builder()
         .status(200)
         .header("x-generated-by", "wasm-workers-server")
