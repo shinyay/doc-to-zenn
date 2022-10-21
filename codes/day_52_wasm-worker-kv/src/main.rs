9b9c0b865e63e8b0d00aa38638df0b648a468e2e
shinyay <shinya.com@gmail.com>
2022-10-21 11:53:49 +0900

    Midify function name

--- codes/day_52_wasm-worker-kv/src/main.rs
+++ codes/day_52_wasm-worker-kv/src/main.rs
@@ -2,7 +2,7 @@ use anyhow::Result;
 use wasm_workers_rs::{handler, http::{self, Request, Response}, cache::Cache};
 
 #[handler(cache)]
-fn handler(req: Request<String>, cache: &mut Cache) -> Result<Response<String>> {
+fn reply(req: Request<String>, cache: &mut Cache) -> Result<Response<String>> {
     Ok(http::Response::builder()
         .status(200)
         .header("x-generated-by", "wasm-workers-server")
