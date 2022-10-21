579998c597c39e48902a9dcd70cef38844fea5b8
shinyay <shinya.com@gmail.com>
2022-10-21 11:51:31 +0900

    Add handler function

--- codes/day_52_wasm-worker-kv/src/main.rs
+++ codes/day_52_wasm-worker-kv/src/main.rs
@@ -1,3 +1,13 @@
-fn main() {
-    println!("Hello, world!");
+use anyhow::Result;
+use wasm_workers_rs::{
+    handler,
+    http::{self, Request, Response}, cache::Cache,
+};
+
+#[handler(cache)]
+fn handler(_req: Request<String>, cache: &mut Cache) -> Result<Response<String>> {
+    Ok(http::Response::builder()
+        .status(200)
+        .header("x-generated-by", "wasm-workers-server")
+        .body(String::from("Hello Wasm!").into())?)
 }
