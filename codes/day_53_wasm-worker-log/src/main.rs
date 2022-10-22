9e5eae0e37ee53d9ff7f356b85274183789650a4
shinyay <shinya.com@gmail.com>
2022-10-22 16:13:10 +0900

    Modify codes

--- codes/day_53_wasm-worker-log/src/main.rs
+++ codes/day_53_wasm-worker-log/src/main.rs
@@ -1,3 +1,14 @@
-fn main() {
-    println!("Hello, world!");
+use anyhow::Result;
+use wasm_workers_rs::{handler, http::{self, Request, Response}};
+use serde_json;
+
+#[handler]
+fn reply(req: Request<String>) -> Result<Response<String>> {
+
+    debug
+    
+    Ok(http::Response::builder()
+    .status(200)
+    .header("x-generated-by", "wasm-workers-server")
+    .body(String::from("Hello Wasm!").into())?)
 }
