9e17b8d8cef440c38364123bbfc5aa226f6edcff
shinyay <shinya.com@gmail.com>
2022-10-24 14:24:31 +0900

    Modify import external module

--- codes/day_55_test-orgnization/src/main.rs
+++ codes/day_55_test-orgnization/src/main.rs
@@ -13,12 +13,17 @@ fn sub(a: i32, b: i32) -> i32 {
     a - b
 }
 
-#[test]
-fn should_added_number_when_two_numbers() {
-    assert_eq!(9, add(3, 6))
-}
+#[cfg(test)]
+mod tests {
+    use super::*;
+    
+    #[test]
+    fn should_added_number_when_two_numbers() {
+        assert_eq!(9, add(3, 6))
+    }
 
-#[test]
-fn should_subtracted_number_when_two_numbers() {
-    assert_eq!(3, sub(6, 3))
+    #[test]
+    fn should_subtracted_number_when_two_numbers() {
+        assert_eq!(3, sub(6, 3))
+    }
 }
