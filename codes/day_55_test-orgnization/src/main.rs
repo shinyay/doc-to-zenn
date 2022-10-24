32bf088e46aaf10a6774c1f867aa79caad3c2412
shinyay <shinya.com@gmail.com>
2022-10-24 13:55:31 +0900

    Add functions for add and substract

--- codes/day_55_test-orgnization/src/main.rs
+++ codes/day_55_test-orgnization/src/main.rs
@@ -1,5 +1,8 @@
 fn main() {
     println!("Hello, world!");
+    println!("2 + 4 = {}", add(2, 4).to_string());
+    println!("4 - 2 = {}", sub(4, 2).to_string());
+
 }
 
 fn add(a: i32, b: i32) -> i32 {
