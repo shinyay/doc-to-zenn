0785f046b2619f8010a22771f4e652803e116334
shinyay <shinya.com@gmail.com>
2022-10-24 14:08:20 +0900

    Add test codes in same module

--- codes/day_55_test-orgnization/src/main.rs
+++ codes/day_55_test-orgnization/src/main.rs
@@ -12,3 +12,13 @@ fn add(a: i32, b: i32) -> i32 {
 fn sub(a: i32, b: i32) -> i32 {
     a - b
 }
+
+#[test]
+fn should_added_number_when_two_numbers() {
+    assert_eq!(9, add(3, 6))
+}
+
+#[test]
+fn should_subtracted_number_when_two_numbers() {
+    assert_eq!(3, sub(6, 3))
+}
