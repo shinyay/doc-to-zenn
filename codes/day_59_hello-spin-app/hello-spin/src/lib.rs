6826819e758dc951fe4e7665f03984d8a9482e5f
shinyay <shinya.com@gmail.com>
2022-10-29 22:23:13 +0900

    Add rust code for spin

--- codes/day_59_hello-spin-app/hello-spin/src/lib.rs
+++ codes/day_59_hello-spin-app/hello-spin/src/lib.rs
@@ -8,8 +8,15 @@ use spin_sdk::{
 #[http_component]
 fn hello_spin(req: Request) -> Result<Response> {
     println!("{:?}", req.headers());
+
+    let query_string = req.uri().query();
+    let reply_body = match query_string {
+        Some(s) => format!("Hello {s}!"),
+        None => "誰ですか？".to_string(),
+    };
+
     Ok(http::Response::builder()
         .status(200)
         .header("foo", "bar")
-        .body(Some("Hello, Fermyon".into()))?)
+        .body(Some(reply_body.into()))?)
 }
