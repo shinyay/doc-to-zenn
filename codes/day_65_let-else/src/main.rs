a7b04ef8db919eee0364c8e718da88c19b20dcd6
shinyay <shinya.com@gmail.com>
2022-11-19 06:37:47 +0900

    Add a scope of let-else

--- codes/day_65_let-else/src/main.rs
+++ codes/day_65_let-else/src/main.rs
@@ -1,9 +1,36 @@
 fn main() {
     let optional = Some(5);
+
     match optional {
         Some(i) => {
             println!("Matched {:?}!", i)
         },
         _ => {},        
     }
+
+    if let Some(i) = optional {
+        println!("Matched {:?}!", i);
+    }
+
+    let result = if let Some(i) = optional {
+        println!("RESULT {:?}!", i);
+    };
+
+    // 変数のxへの束縛範囲
+
+    // if-let文
+    if let Some(x) = Some(3) { 
+        // ------↓ここから束縛↓------
+        assert_eq!(x, 3);
+        // ------↑ここまで↑------
+    } else {}
+
+    // let-else文
+    let Some(x) = Some(4) else {
+        return
+    };
+    // ------↓ここから束縛↓------
+    assert_eq!(x, 4);
+
+
 }
