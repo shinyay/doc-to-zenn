12043c799b41c2d0163c03f068608c28dd694326
shinyay <shinya.com@gmail.com>
2022-11-30 16:35:43 +0900

    Add for loop

--- codes/day_73_rand/src/main.rs
+++ codes/day_73_rand/src/main.rs
@@ -3,7 +3,10 @@ use rand::Rng;
 fn main() {
 
     let mut rng = rand::thread_rng();
-    let x: i32 = rng.gen();
-    println!("{}", x);
-    println!("{:?}", rng.gen::<(f64, bool)>());    
+   
+    for n in 1..10 {
+        let x: i32 = rng.gen();
+        println!("{}", x);
+    }
+    println!("{:?}", rng.gen::<(f64, bool)>());
 }
