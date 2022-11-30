f3e2aa8cf4d11e2ab1cbd0be33699928e55661e2
shinyay <shinya.com@gmail.com>
2022-11-30 19:30:10 +0900

    Add shuffle method

--- codes/day_73_rand/src/main.rs
+++ codes/day_73_rand/src/main.rs
@@ -15,4 +15,9 @@ fn main() {
     for n in 1..10 {
             println!("{}, {:?}",n, choices.choose(&mut rng));
     }
+
+    let mut y = [1, 2, 3, 4, 5];
+    println!("シャッフル前: {:?}", y);
+    y.shuffle(&mut rng);
+    println!("シャッフル後: {:?}", y);
 }
