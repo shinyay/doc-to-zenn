22b15af41398267274da3a22e4240d6b088a809a
shinyay <shinya.com@gmail.com>
2022-11-30 14:58:43 +0900

    Add thread_rng

--- codes/day_73_rand/src/main.rs
+++ codes/day_73_rand/src/main.rs
@@ -1,3 +1,9 @@
+use rand::Rng;
+
 fn main() {
-    println!("Hello, world!");
+
+    let mut rng = rand::thread_rng();
+    let x: f64 = rng.gen();
+    println!("{}", x);
+    println!("{:?}", rng.gen::<(f64, bool)>());    
 }
