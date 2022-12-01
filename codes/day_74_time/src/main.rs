8f262399265fa33fd85bbc3b6eb071f8da1a6374
shinyay <shinya.com@gmail.com>
2022-12-02 08:54:15 +0900

    Add local-offset

--- codes/day_74_time/src/main.rs
+++ codes/day_74_time/src/main.rs
@@ -2,4 +2,7 @@ use time::OffsetDateTime;
 fn main() {
     let now = OffsetDateTime::now_utc();
     println!("Hello, world at {now}");
+
+    let now = OffsetDateTime::now_local();
+    println!("Hello, world at {:?}", now);
 }
