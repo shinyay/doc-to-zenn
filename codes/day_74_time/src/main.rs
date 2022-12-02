e7103f8980601180249aea3e57fb33bc0ecceb7c
shinyay <shinya.com@gmail.com>
2022-12-02 11:23:34 +0900

    Add PrimitiveDateTime

--- codes/day_74_time/src/main.rs
+++ codes/day_74_time/src/main.rs
@@ -1,4 +1,4 @@
-use time::{OffsetDateTime, Date, Time};
+use time::{OffsetDateTime, Date, Time, PrimitiveDateTime, macros::datetime};
 fn main() {
     let now = OffsetDateTime::now_utc();
     println!("Hello, world at {now}");
@@ -7,9 +7,14 @@ fn main() {
     println!("Hello, world at {:?}", now);
 
     let date = Date::from_iso_week_date(2022, 48, time::Weekday::Friday).unwrap();
-    let datetime = date.with_hms(11, 22, 33).unwrap();
+    let datetime: PrimitiveDateTime = date.with_hms(11, 22, 33).unwrap();
     let time = Time::from_hms(11, 22, 33).unwrap();
     println!("Date: {date}");
     println!("Time: {time}");
     println!("DateTime: {datetime}");
+
+    let start = datetime!(2022-01-01 0:00:00);
+    let end = datetime!(2022-12-31 23:59:59);
+    let duration = end - start;
+    println!("{duration}");
 }
