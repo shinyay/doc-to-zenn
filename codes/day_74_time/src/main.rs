5829e165e340e0b4a3c6dcfeffab48505639e23f
shinyay <shinya.com@gmail.com>
2022-12-02 11:00:06 +0900

    Add Time

--- codes/day_74_time/src/main.rs
+++ codes/day_74_time/src/main.rs
@@ -1,8 +1,15 @@
-use time::OffsetDateTime;
+use time::{OffsetDateTime, Date, Time};
 fn main() {
     let now = OffsetDateTime::now_utc();
     println!("Hello, world at {now}");
 
     let now = OffsetDateTime::now_local();
     println!("Hello, world at {:?}", now);
+
+    let date = Date::from_iso_week_date(2022, 48, time::Weekday::Friday).unwrap();
+    let datetime = date.with_hms(11, 22, 33).unwrap();
+    let time = Time::from_hms(11, 22, 33).unwrap();
+    println!("Date: {date}");
+    println!("Time: {time}");
+    println!("DateTime: {datetime}");
 }
