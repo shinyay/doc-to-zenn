e1abf75f44d8d49b91fc9cdde5093468a552db57
shinyay <shinya.com@gmail.com>
2022-12-20 15:44:46 +0900

    Modify typo

--- codes/day_91_hello_axum/src/main.rs
+++ codes/day_91_hello_axum/src/main.rs
@@ -7,7 +7,7 @@ use axum::routing::get;
 async fn main() {
 
     let app = Router::new().route("/", get(root));
-    let addr = SocketAddr::from(([127, 9, 0, 1], 3000));
+    let addr = SocketAddr::from(([127, 0, 0, 1], 3000));
 
     axum::Server::bind(&addr)
         .serve(app.into_make_service())
