bfca487575f32194cca29f4c77e2cae30d12c8c4
shinyay <shinya.com@gmail.com>
2022-12-21 12:43:51 +0900

    Add logging

--- codes/day_92_hello_axum/src/main.rs
+++ codes/day_92_hello_axum/src/main.rs
@@ -10,9 +10,10 @@ async fn main() {
     let log_level = env::var("RUST_LOG").unwrap_or("info".to_string());
     env::set_var("RUST_LOG", log_level);
     tracing_subscriber::fmt::init();
-    
+
     let app = Router::new().route("/", get(root));
     let addr = SocketAddr::from(([127, 0, 0, 1], 3000));
+    tracing::debug!("listening on {}", addr);
 
     axum::Server::bind(&addr)
         .serve(app.into_make_service())
