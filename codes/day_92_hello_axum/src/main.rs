25fe079760de54be91a8339832808f783be9767e
shinyay <shinya.com@gmail.com>
2022-12-21 12:34:52 +0900

    Add axum hello world

---
+++ codes/day_92_hello_axum/src/main.rs
@@ -0,0 +1,20 @@
+use std::net::SocketAddr;
+
+use axum::Router;
+use axum::routing::get;
+
+#[tokio::main]
+async fn main() {
+
+    let app = Router::new().route("/", get(root));
+    let addr = SocketAddr::from(([127, 0, 0, 1], 3000));
+
+    axum::Server::bind(&addr)
+        .serve(app.into_make_service())
+        .await
+        .unwrap();
+}
+
+async fn root() -> &'static str {
+    "Hello, axum!"
+}
