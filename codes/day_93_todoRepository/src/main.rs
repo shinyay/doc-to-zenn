32e3c061100a40e9b76b5b8bb3ec538704b893f7
shinyay <shinya.com@gmail.com>
2022-12-22 18:31:49 +0900

    Add struct for repository

--- codes/day_93_todoRepository/src/main.rs
+++ codes/day_93_todoRepository/src/main.rs
@@ -8,7 +8,8 @@ enum RepositoryError {
 }
 
 pub trait TodoRepository: Clone + std::marker::Send + std::marker::Sync + 'static {
-    fn create(&self, payload: CreateTodo) -> ToDo;    
+    fn create(&self, payload: CreateTodo) -> ToDo;
+    
 }
 
 #[derive(Debug, Serialize, Deserialize, Clone, PartialEq, Eq)]
@@ -28,6 +29,16 @@ pub struct CreateTodo {
 pub struct UpdateTodo {
     text: Option<String>,
     completed: Option<bool>,
+}
+
+impl Todo {
+    pub fn new(id: i32, text: String) -> Self {
+        Self { 
+            id, 
+            text, 
+            completed: false,
+        }
+    }
 }
 
 fn main() {
