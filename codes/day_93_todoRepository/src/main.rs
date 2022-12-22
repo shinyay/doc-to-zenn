a32680adff5a876de9588b60e92914cd523f32fa
shinyay <shinya.com@gmail.com>
2022-12-22 16:41:33 +0900

    Add enum for error

--- codes/day_93_todoRepository/src/main.rs
+++ codes/day_93_todoRepository/src/main.rs
@@ -1,3 +1,11 @@
+use thiserror::Error;
+
+#[derive(Debug, Error)]
+enum RepositoryError {
+    #[error("NotFound, id is {0}")]
+    NotFound(i32),
+}
+
 fn main() {
     println!("Hello, world!");
 }
