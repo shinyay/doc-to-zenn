b7b962a0af151c8e302b77274cdf41bad35bf574
shinyay <shinya.com@gmail.com>
2023-02-06 15:25:41 +0900

    Add rust project

---
+++ codes/wasm-docker-helloworld/target/debug/deps/wasm_docker_helloworld-5e6372eda2a51e7b.d
@@ -0,0 +1,5 @@
+/Users/yanagiharas/Works/docs/doc-to-zenn/codes/wasm-docker-helloworld/target/debug/deps/wasm_docker_helloworld-5e6372eda2a51e7b: src/main.rs
+
+/Users/yanagiharas/Works/docs/doc-to-zenn/codes/wasm-docker-helloworld/target/debug/deps/wasm_docker_helloworld-5e6372eda2a51e7b.d: src/main.rs
+
+src/main.rs:
---
+++ codes/wasm-docker-helloworld/target/debug/wasm-docker-helloworld.d
@@ -0,0 +1,1 @@
+/Users/yanagiharas/Works/docs/doc-to-zenn/codes/wasm-docker-helloworld/target/debug/wasm-docker-helloworld: /Users/yanagiharas/Works/docs/doc-to-zenn/codes/wasm-docker-helloworld/src/main.rs
